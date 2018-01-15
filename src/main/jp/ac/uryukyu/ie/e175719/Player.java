3a642c732e76bf12fd2678f92f29c701c6b8dfb1
Yuiri Arakaki <e175719@ie.u-ryukyu.ac.jp>
2018-01-15 22:19:18 +0900

    first commit

---
+++ src/main/jp/ac/uryukyu/ie/e175719/Player.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e175719;
+
+public class Player {
+    String name;
+
+
+    public String name(){
+        return name;
+    }
+
+    public Player(String name){
+        this.name = name;
+    }
+
+}
