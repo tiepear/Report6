333c7a7f22c860834bee664b168d0c632ad0971c
Yuiri Arakaki <e175719@ie.u-ryukyu.ac.jp>
2018-02-06 00:56:40 +0900

    commit2

--- src/main/java/jp/ac/uryukyu/ie/e175719/Judge.java
+++ src/main/java/jp/ac/uryukyu/ie/e175719/Judge.java
@@ -6,7 +6,6 @@ public class Judge {
     public void playGame(Card game , Player user1, Player user2){
         ArrayList<String> handCard1 = game.playerCard1;
         ArrayList<String> handCard2 = game.playerCard2;
-
         String card;
 
         while(true){
@@ -29,7 +28,6 @@ public class Judge {
                 handCard1.remove(card);
             }
 
-
             if(handCard1.size() == 0){
                 System.out.println("勝者:" + user1.name);
                 System.out.println("敗者:" + user2.name);
@@ -41,17 +39,12 @@ public class Judge {
                 break;
             }
 
-
             card = handCard1.get((int)(Math.random()* handCard1.size()));
             if (handCard2.indexOf(card) == -1) {
                 handCard2.add(card);
             } else {
                 handCard2.remove(card);
             }
-
         }
-
-
     }
-
 }
